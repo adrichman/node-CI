efdf0423b3818af932ed5151fa79fd4c6a0593ca
Adam D Richman <ar@adamrichman.com>
2014-06-12 13:28:35 -0400

    fix paths in script

--- restarter.sh
+++ restarter.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# in crontab: @reboot ~/srv/ciServer/restarter.sh >> cron.log 2>&1
+# in crontab: @reboot /srv/ciServer/restarter.sh >> cron.log 2>&1
 
 if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
-        forever start ~/srv/ngLazy-demo/server.js > /dev/null
-        forever start ~/srv/ciServer/ciServer.js > /dev/null
+        forever start /srv/ngLazy-demo/server.js > /dev/null
+        forever start /srv/ciServer/ciServer.js > /dev/null
 fi
