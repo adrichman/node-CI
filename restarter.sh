c49a6495442833686b90661977eb266e2a42cfd4
Adam D Richman <ar@adamrichman.com>
2014-06-12 13:28:35 -0400

    fix paths in script

--- restarter.sh
+++ restarter.sh
@@ -3,6 +3,6 @@
 
 if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
-        forever start ~/srv/ngLazy-demo/server.js > /dev/null
-        forever start ~/srv/ciServer/ciServer.js > /dev/null
+        forever start /srv/ngLazy-demo/server.js > /dev/null
+        forever start /srv/ciServer/ciServer.js > /dev/null
 fi
